d0e5b14fcb3f7d57e80b147a021adf54828d9109
kasund <kasund@hsenidlanka.com>
2019-11-27 11:42:53 +0530

    profile image added

--- lib/main.dart
+++ lib/main.dart
@@ -34,12 +34,69 @@ class _MyHomePageState extends State<MyHomePage>{
                   Container(
                     height: 200.0,
                     width: double.infinity,
-                    color: Colors.purpleAccent,
+                    color: Colors.purple,
+                  ),
+                  Positioned(
+                    bottom: 55.0,
+                    right: 100.0,
+                    child: Container(
+                      height: 350.0,
+                      width: 350.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(320),
+                        color: Colors.deepPurpleAccent
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 100.0,
+                    left: 130.0,
+                    child: Container(
+                      height: 290.0,
+                      width: 300.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(320),
+                        color: Colors.pinkAccent
+                      ),
+                    ),
+                  ),
+                  Column(
+                    //crossAllignment
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 14.0,
+
+                      ),
+                      Row(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 14.0,
+                          ),
+                          Container(
+                            alignment: Alignment.topLeft,
+                            height: 50.0,
+                            width: 50.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(
+                                color: Colors.white,
+                                style: BorderStyle.solid,
+                                width: 2.0
+                              ),
+                              image: DecorationImage(
+                                image: AssetImage('images/profile.jpg'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   )
                 ],
-              )
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
